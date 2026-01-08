7db3bae3f51544f07ee6369d800e1412c8103a11
ChepeRestrepo <joserestrepo2901@gmail.com>
2026-01-07 19:25:23 -0500

    Se agrega el cuerpo, el estilo y la logica al chat bot

---
+++ script.js
@@ -0,0 +1,66 @@
+let jokeIndex = 0;
+const jokes = [
+  ["What did 0 say to 8?", "Nice belt."],
+  ["Cuanto es 1 x 2", "Es obvio que 4"],
+];
+
+const chatContent = document.querySelector(".chat-content");
+const jokeButton = document.getElementById("requestJokeButton");
+
+function appendBotMessage(messageText) {
+  const messageDiv = document.createElement("div");
+  messageDiv.className = "message bot-message";
+
+  const avatar = document.createElement("div");
+  avatar.className = "fas fa-robot message-avatar";
+
+  const contentDiv = document.createElement("div");
+  contentDiv.className = "message-content";
+  contentDiv.textContent = messageText;
+
+  messageDiv.append(avatar, contentDiv);
+  chatContent.appendChild(messageDiv);
+
+  chatContent.scrollTop = chatContent.scrollHeight;
+}
+
+function appendUserMessage() {
+  const messageDiv = document.createElement("div");
+  messageDiv.className = "message user-message";
+
+  const avatar = document.createElement("div");
+  avatar.className = "fas fa-smile message-avatar";
+
+  const contentDiv = document.createElement("div");
+  contentDiv.className = "message-content";
+  contentDiv.textContent = "Tell me a joke!";
+
+  messageDiv.append(avatar, contentDiv);
+  chatContent.appendChild(messageDiv);
+}
+
+appendBotMessage(
+  "Hello! I am Comedian Bot, here to make you laugh. Let me think of a joke."
+);
+
+function requestJoke() {
+  appendUserMessage();
+  if (jokeIndex >= jokes.length) {
+    appendBotMessage("Sorry, I'm out of jokes for now!");
+    return;
+  }
+  jokeButton.style.display = "none";
+  setTimeout(function () {
+    appendBotMessage("Ok, got one.");
+  }, 1000);
+
+  setTimeout(function () {
+    appendBotMessage(jokes[jokeIndex][0]);
+  }, 1500);
+
+  setTimeout(function () {
+    appendBotMessage(jokes[jokeIndex][1]);
+    jokeButton.style.display = "inline-block";
+    jokeIndex++;
+  }, 2000);
+}
